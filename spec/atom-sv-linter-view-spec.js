82e554d60a15738fb7916ed38acba56d4e147325
Koen Goetschalckx <kgoetsch@mimas.esat.kuleuven.be>
2018-10-02 10:02:10 +0200

    cleaning up

--- spec/atom-sv-linter-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomvcsLinterView from '../lib/atom-vcs-linter-view';
-
-describe('AtomvcsLinterView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
